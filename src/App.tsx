5cddc304f36597fcd0652ecb82a1511cae7c5db4
= <=>
2023-01-11 10:14:40 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { AppTheme, baseTheme } from './design-system/theme';
+import func from './store';
+import { history } from './history';
+
+import { BrowserRouter as Router } from './overrides/BrowserRouter';
+import EntryPoint from './modules';
+
+const GlobalStyle = createGlobalStyle<{ theme: AppTheme }>`
+  html, body {
+    margin: 0;
+    padding: 0;
+    background-color: #F8F8F8;
+    font-family: Rubik;
+    font-size: 0.9rem
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+      color: ${({ theme }) => theme.colors.darkOrange};
+    }
+    &:visited {
+      color: ${({ theme }) => theme.colors.darkGreen};
+    }
+  }
+
+  // Newer semantic elements don't have block styling in IE11
+  article, header, nav, section, footer, aside {
+    display: block;
+  }
+`;
+
+const { store, persistor } = func();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={baseTheme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+        <Router navigator={history}>
+            <EntryPoint />
+        </Router>
+        </PersistGate>
+      </Provider>
+
+    </ThemeProvider>
   );
 }
 
